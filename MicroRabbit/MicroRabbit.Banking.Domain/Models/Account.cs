08784c0b3e599439e3e2a0d7c6754bbb5ea7f3f1
Pulkit Kumawat <pulkit.kumawat95@gmail.com>
2020-12-23 10:28:22 +0530

    partially completed Banking microservice

---
+++ MicroRabbit/MicroRabbit.Banking.Domain/Models/Account.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Banking.Domain.Models
+{
+    public class Account
+    {
+        public int id { get; set; }
+        public string AccountType1 { get; set; }
+        public decimal AccountBalance { get; set; }
+    }
+}
